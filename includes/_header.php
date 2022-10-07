4de5998f672f7d3a893e7ef6f5a017c328af28e0
HenriqueCCruz <99992300+CRUZ1807@users.noreply.github.com>
2022-10-07 08:46:34 -0300

    Add files via upload

---
+++ includes/_header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>JORNAL</title>
+  </head>
+  <body class="container-fluid">
+    <header class="row">
+      <img src="./imgs/pngwing.com.png" alt="">
+      <h1>MUNDO ECONÔMICO</h1>
+        <div>
+            <input class="col-lg-6" type="text">
+            <a href="">ENTRAR</a>
+        </div>
+    </header>
+
+    <?php
+    include_once './index.php';
+
+    
