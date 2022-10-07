d05af98a8330d9f1a6a761af77c41dca3e9b3194
EllenVargas <henriquecc2006@gmail.com>
2022-10-07 07:51:16 -0300

    skksjdjknskjdcnscd

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>JORNAL</title>
+  </head>
+  <body class="container-fluid">
+    <header class="row">
+        <h1>MUNDO ECONÔMICO</h1>
+        <div>
+            <input class="col-lg-6" type="text">
+            <a href="">LOG IN</a>
+        </div>
+    </header>
+
+    
