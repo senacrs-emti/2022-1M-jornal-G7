81d3734d4ace874e80e5c5ac585578ef46361706
anacarolinavoto <henriquecc2006@gmail.com>
2022-10-20 09:25:10 -0300

    header iniciado

--- header-footer/header.php
+++ header-footer/header.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    
-</head>
-<body>
-    
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!--nosso css-->
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+        
+    </header>
+    <script src="script.js"></script>
+  </body>
 </html>
