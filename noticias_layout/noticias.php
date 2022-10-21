c4209b824e3d34838c69e0b0ee0bb2933916ce29
Júlia <juliawainerborges0@gmail.com>
2022-10-21 08:29:26 -0300

    noticias.php

---
+++ noticias_layout/noticias.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!--nosso css-->
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <?php
+        include_once './header-footer/header.php';
+    ?>
+
+    <main>
+        <h1>Título Chamativo<h1>
+        <h3>Sub. Título<h3>
+        <h5>Data:27/07/2006<h5>
+        <hr>
+        <img src="..." class="img-fluid" alt="...">
+
+    </main>
+    <script src="script.js"></script>
+  </body>
+</html>
