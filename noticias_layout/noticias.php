7e9ce79797223e7c94ada5bc8bfd63a521783c9a
Júlia <juliawainerborges0@gmail.com>
2022-10-21 09:59:03 -0300

    noticia
    não mudou nada

--- noticias_layout/noticias.php
+++ noticias_layout/noticias.php
@@ -6,28 +6,29 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!--nosso css-->
-    <link rel="stylesheet" href="noticias.css">
+    <link rel="stylesheet" href="./noticias_layout/noticias.css">
   </head>
   <body>
     <?php
         include_once 'C:\xampp\htdocs\2022-1M-jornal-G7\header-footer\header.php';
     ?>
-    
     <main>
-        <h1 class="text-center">Título Chamativo<h1>
-        <h3 class="text-center">Sub. Título<h3>
-        <h5 class="text-center">Data:27/07/2006<h5>
-        <hr>
-        
-        <article id="container-article">
+        <div>
+            <h1 class="text-center">Título Chamativo<h1>
+            <h3 class="text-center">Sub. Título<h3>
+            <h5 class="text-center">Data:27/07/2006<h5>
+        </div>
+    <hr>
+
+    <article id="container-article">
             <img src="https://www.tbsconsultoria.com.br/wp-content/uploads/2019/01/265919-redator-entregar-ate-2812-as-10heconomia-mundial-quais-sao-as-10-tendencias-para-os-proximos-anos.jpg" class="img-fluid" alt="...">
+            <img src="https://www.blbbrasil.com.br/blog/wp-content/uploads/2020/10/blb-ventures-nova-economia.png" class="rounded float-end" alt="...">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim a massa auctor accumsan. Ut dui nibh, ullamcorper nec feugiat a, eleifend sed eros. Suspendisse ultricies quam ut risus vehicula maximus. Suspendisse sed posuere massa. Ut sed interdum mauris. Sed scelerisque consectetur dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec nec mi ligula. Praesent at dolor eu urna bibendum dignissim a nec felis.</p>
-            <img src="..." class="rounded float-end" alt="...">
             <p>Praesent at eros quis mauris faucibus tincidunt dictum quis libero. Nullam sit amet lectus et massa luctus ultrices. Etiam efficitur feugiat neque. Nunc arcu quam, lacinia sed augue eget, pharetra condimentum neque. Praesent tincidunt fermentum metus a eleifend. Quisque sollicitudin ornare ipsum sed consectetur. Donec non pharetra neque, vitae maximus leo. Mauris tristique et est a semper. In aliquam felis eu tortor consectetur pulvinar. Nullam aliquet neque vel bibendum elementum. Nunc tincidunt malesuada consectetur. Integer magna dolor, auctor et lectus et, iaculis condimentum ante. Pellentesque nisl purus, aliquam ut lorem malesuada, molestie luctus orci. Donec commodo facilisis aliquam. Nulla mollis placerat augue, ut elementum leo dapibus at.</p>
-            <img src="..." class="img-fluid" alt="...">
+            <img src="https://www.tbsconsultoria.com.br/wp-content/uploads/2019/01/265919-redator-entregar-ate-2812-as-10heconomia-mundial-quais-sao-as-10-tendencias-para-os-proximos-anos.jpg" class="img-fluid" alt="...">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sapiente excepturi quae atque exercitationem molestiae eligendi consequuntur quibusdam ipsa repudiandae nostrum at earum consectetur, possimus optio odit corporis! Eaque,Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ab sunt tenetur libero asperiores laboriosam iusto quisquam ipsa enim doloribus magni aliquid veritatis perferendis eligendi, esse praesentium minus iure quam!</p>
             <hr>
-        </article>
+    </article>
 
             <div class="card mb-3" style="max-width: 540px;">
                 <div class="row g-0">
