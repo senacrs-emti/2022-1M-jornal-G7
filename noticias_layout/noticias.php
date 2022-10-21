4ce3fd2216421d61f385a606ebbe111ddac7fba1
Ethan <ethan29ssr@gmail.com>
2022-10-21 08:36:08 -0300

    Inclusões e modificações
    Correção de xampp e css

--- noticias_layout/noticias.php
+++ noticias_layout/noticias.php
@@ -6,13 +6,13 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!--nosso css-->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="noticias.css">
   </head>
   <body>
     <?php
-        include_once './header-footer/header.php';
+        include_once 'C:\xampp\htdocs\2022-1M-jornal-G7\header-footer\header.php';
     ?>
-
+    
     <main>
         <h1>Título Chamativo<h1>
         <h3>Sub. Título<h3>
