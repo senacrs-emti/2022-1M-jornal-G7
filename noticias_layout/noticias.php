c541a32a0915d4953cba147a901402757f39db28
Júlia <juliawainerborges0@gmail.com>
2022-10-21 09:18:01 -0300

    Noticia.php_3
    Terminei a base em html

---
+++ noticias_layout/noticias.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!--nosso css-->
+    <link rel="stylesheet" href="noticias.css">
+  </head>
+  <body>
+    <?php
+        include_once 'C:\xampp\htdocs\2022-1M-jornal-G7\header-footer\header.php';
+    ?>
+    
+    <main>
+        <h1>Título Chamativo<h1>
+        <h3>Sub. Título<h3>
+        <h5>Data:27/07/2006<h5>
+        <hr>
+        <img src="..." class="img-fluid" alt="...">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim a massa auctor accumsan. Ut dui nibh, ullamcorper nec feugiat a, eleifend sed eros. Suspendisse ultricies quam ut risus vehicula maximus. Suspendisse sed posuere massa. Ut sed interdum mauris. Sed scelerisque consectetur dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec nec mi ligula. Praesent at dolor eu urna bibendum dignissim a nec felis.</p>
+        <img src="..." class="rounded float-end" alt="...">
+        <p>Praesent at eros quis mauris faucibus tincidunt dictum quis libero. Nullam sit amet lectus et massa luctus ultrices. Etiam efficitur feugiat neque. Nunc arcu quam, lacinia sed augue eget, pharetra condimentum neque. Praesent tincidunt fermentum metus a eleifend. Quisque sollicitudin ornare ipsum sed consectetur. Donec non pharetra neque, vitae maximus leo. Mauris tristique et est a semper. In aliquam felis eu tortor consectetur pulvinar. Nullam aliquet neque vel bibendum elementum. Nunc tincidunt malesuada consectetur. Integer magna dolor, auctor et lectus et, iaculis condimentum ante. Pellentesque nisl purus, aliquam ut lorem malesuada, molestie luctus orci. Donec commodo facilisis aliquam. Nulla mollis placerat augue, ut elementum leo dapibus at.</p>
+        <img src="..." class="img-fluid" alt="...">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sapiente excepturi quae atque exercitationem molestiae eligendi consequuntur quibusdam ipsa repudiandae nostrum at earum consectetur, possimus optio odit corporis! Eaque,Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ab sunt tenetur libero asperiores laboriosam iusto quisquam ipsa enim doloribus magni aliquid veritatis perferendis eligendi, esse praesentium minus iure quam!</p>
+        <hr>
+
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="..." class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h3 class="card-title">Título</h3>
+                        <h4 class="card-title">Sub. Título</h4>
+                        <h5 class="card-text">Data:27/07/2006</h5>
+                     </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="..." class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h3 class="card-title">Título</h3>
+                        <h4 class="card-text">Sub. Título</h4>
+                        <h5 class="card-text">Data:27/07/2006</h5>
+                     </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="..." class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h3 class="card-title">Título</h3>
+                        <h4 class="card-text">Sub. Título</h4>
+                        <h5 class="card-text">Data:27/07/2006</h5>
+                     </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <script src="script.js"></script>
+  </body>
+</html>
