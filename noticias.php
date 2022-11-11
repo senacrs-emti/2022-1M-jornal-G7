a10a87cd451ad66fbf8cf0dc695ff35526203a83
Ethan <ethan29ssr@gmail.com>
2022-11-11 08:46:29 -0300

    Alteração
    Alteração de layout e tamanhos

--- noticias.php
+++ noticias.php
@@ -2,17 +2,17 @@
         include_once '_header.php';
     ?>
     
-    <main class="row">
-        <div class="col-10 ">
+    <main>
+        <div>
             <h1 class="text-center"><b>Título Chamativo</b><h1>
             <h3 class="text-center">Sub. Título<h3>
             <h6>Data:27/07/2006<h5>
             <hr>
-            <img class="img_N" src="..." alt="..." class="w-100">
-            <img src="https://cdn.xxl.thumbs.canstockphoto.com.br/cora%C3%A7%C3%A3o-formando-imagem-dois-panor%C3%A2mico-m%C3%A3os-s%C3%ADmbolo-banco-de-fotos_csp23260933.jpg" class="rounded float-end" alt="...">
+            <img class="img_w100" src="https://img.olhardigital.com.br/wp-content/uploads/2018/09/20180903133856-860x450.jpg" alt="...">
+            <!--img src="https://cdn.xxl.thumbs.canstockphoto.com.br/cora%C3%A7%C3%A3o-formando-imagem-dois-panor%C3%A2mico-m%C3%A3os-s%C3%ADmbolo-banco-de-fotos_csp23260933.jpg" class="rounded float-end" alt="..."-->
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam tempore sequi perferendis consequatur perspiciatis delectus iure ducimus blanditiis iste omnis suscipit voluptatem facere eaque earum ut, ea debitis repudiandae! Assumenda! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, inventore deleniti provident atque rem ex aliquam perferendis temporibus, doloremque delectus placeat quis dolore dicta saepe voluptatem accusantium dolorum architecto iste!Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe doloribus ea quidem magnam maiores iure. Voluptas dolores architecto porro eum accusantium dolorum sint, quo nobis voluptatum adipisci similique unde laboriosam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab excepturi aliquid velit dolor, esse nemo commodi recusandae quam aliquam molestiae perferendis voluptatum nesciunt iusto iste officia aut adipisci possimus. Odio! Lorem ipsum dolor sit amet consectetur adipisicing.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis molestias accusamus officia blanditiis tempora reiciendis quasi, nulla error quo! Saepe eius obcaecati laborum magnam itaque tempore perspiciatis voluptatum minus quasi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus atque, quae facere quo soluta obcaecati mollitia est corporis delectus repellendus sint consequatur! Similique repellendus molestias ab omnis enim deleniti?</p>
-            <img class="img_N" src="https://st.depositphotos.com/1010338/2099/i/450/depositphotos_20999947-stock-photo-tropical-island-with-palms.jpg" class="img-fluid" alt="...">
+            <img class="img_w100" src="https://st.depositphotos.com/1010338/2099/i/450/depositphotos_20999947-stock-photo-tropical-island-with-palms.jpg" class="img-fluid" alt="...">
             
         </div>  
         <hr>
