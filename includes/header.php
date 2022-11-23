73c02dcb6484d26f291e9b9859cdefa3e50bbb0d
CRUZ1807 <henriquecc2006@gmail.com>
2022-11-23 08:55:32 -0300

    header ajeitado

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS Versão 4.6.0-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <!-- Nosso CSS-->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="includes/header.css">
 
   </head>
   <body>
