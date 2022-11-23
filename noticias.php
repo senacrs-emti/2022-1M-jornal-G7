1776f6d88db115d38869606e9082b85735f33591
DogeSOVIET <ethan29ssr@gmail.com>
2022-11-22 21:17:51 -0300

    AD corpo
    Organização do código e melhora no visual do corpo

--- noticias.php
+++ noticias.php
@@ -2,69 +2,68 @@
         include_once '_header.php';
     ?>
     
-    <main>
+    <main id="notMAIN">
         <div>
             <h1 class="text-center"><b>Título Chamativo</b><h1>
             <h3 class="text-center">Sub. Título<h3>
             <h6>Data:27/07/2006<h5>
             <hr>
-            <img class="img_w100" src="https://img.olhardigital.com.br/wp-content/uploads/2018/09/20180903133856-860x450.jpg" alt="...">
-            <!--img src="https://cdn.xxl.thumbs.canstockphoto.com.br/cora%C3%A7%C3%A3o-formando-imagem-dois-panor%C3%A2mico-m%C3%A3os-s%C3%ADmbolo-banco-de-fotos_csp23260933.jpg" class="rounded float-end" alt="..."-->
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam tempore sequi perferendis consequatur perspiciatis delectus iure ducimus blanditiis iste omnis suscipit voluptatem facere eaque earum ut, ea debitis repudiandae! Assumenda! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, inventore deleniti provident atque rem ex aliquam perferendis temporibus, doloremque delectus placeat quis dolore dicta saepe voluptatem accusantium dolorum architecto iste!Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe doloribus ea quidem magnam maiores iure. Voluptas dolores architecto porro eum accusantium dolorum sint, quo nobis voluptatum adipisci similique unde laboriosam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab excepturi aliquid velit dolor, esse nemo commodi recusandae quam aliquam molestiae perferendis voluptatum nesciunt iusto iste officia aut adipisci possimus. Odio! Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis molestias accusamus officia blanditiis tempora reiciendis quasi, nulla error quo! Saepe eius obcaecati laborum magnam itaque tempore perspiciatis voluptatum minus quasi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus atque, quae facere quo soluta obcaecati mollitia est corporis delectus repellendus sint consequatur! Similique repellendus molestias ab omnis enim deleniti?</p>
-            <img class="img_w100" src="https://st.depositphotos.com/1010338/2099/i/450/depositphotos_20999947-stock-photo-tropical-island-with-palms.jpg" class="img-fluid" alt="...">
-            
+            <img class="notIMG" src="https://img.olhardigital.com.br/wp-content/uploads/2018/09/20180903133856-860x450.jpg" alt="...">
+            <aside class="mx-5">
+                <img src="https://cdn.xxl.thumbs.canstockphoto.com.br/cora%C3%A7%C3%A3o-formando-imagem-dois-panor%C3%A2mico-m%C3%A3os-s%C3%ADmbolo-banco-de-fotos_csp23260933.jpg" class="rounded float-end" alt="...">
+            </aside>
+            <p class="mx-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam tempore sequi perferendis consequatur perspiciatis delectus iure ducimus blanditiis iste omnis suscipit voluptatem facere eaque earum ut, ea debitis repudiandae! Assumenda! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, inventore deleniti provident atque rem ex aliquam perferendis temporibus, doloremque delectus placeat quis dolore dicta saepe voluptatem accusantium dolorum architecto iste!Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe doloribus ea quidem magnam maiores iure. Voluptas dolores architecto porro eum accusantium dolorum sint, quo nobis voluptatum adipisci similique unde laboriosam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab excepturi aliquid velit dolor, esse nemo commodi recusandae quam aliquam molestiae perferendis voluptatum nesciunt iusto iste officia aut adipisci possimus. Odio! Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            <p class="mx-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis molestias accusamus officia blanditiis tempora reiciendis quasi, nulla error quo! Saepe eius obcaecati laborum magnam itaque tempore perspiciatis voluptatum minus quasi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae possimus atque, quae facere quo soluta obcaecati mollitia est corporis delectus repellendus sint consequatur! Similique repellendus molestias ab omnis enim deleniti?</p>
+            <img class="notIMG" src="https://st.depositphotos.com/1010338/2099/i/450/depositphotos_20999947-stock-photo-tropical-island-with-palms.jpg" alt="...">            
         </div>  
         <hr>
+        <div class="row">
+                <div class="card mb-3" style="max-width: 540px;" style="width: 200px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="https://www.ladrilar.com.br/wp-content/uploads/2020/12/cinza-escuro.png" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h3 class="card-title text-center">Título</h3>
+                                <h4 class="card-title text-center">Sub. Título</h4>
+                                <a href="#" class="stretched-link"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="https://www.ladrilar.com.br/wp-content/uploads/2020/12/cinza-escuro.png" class="img-fluid rounded-start " alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h3 class="card-title text-center">Título</h3>
+                                <h4 class="card-text text-center">Sub. Título</h4>
+                                <a href="#" class="stretched-link"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="https://www.ladrilar.com.br/wp-content/uploads/2020/12/cinza-escuro.png" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h3 class="card-title text-center">Título</h3>
+                                <h4 class="card-text text-center">Sub. Título</h4>
+                                <a href="#" class="stretched-link"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </main>
-    
-    <div class="row">
-            <div class="card mb-3" style="max-width: 540px;" style="width: 200px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://www.ladrilar.com.br/wp-content/uploads/2020/12/cinza-escuro.png" class="img-fluid rounded-start" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h3 class="card-title text-center">Título</h3>
-                            <h4 class="card-title text-center">Sub. Título</h4>
-                            <a href="#" class="stretched-link"></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <hr>
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://www.ladrilar.com.br/wp-content/uploads/2020/12/cinza-escuro.png" class="img-fluid rounded-start" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h3 class="card-title text-center">Título</h3>
-                            <h4 class="card-text text-center">Sub. Título</h4>
-                            <a href="#" class="stretched-link"></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <hr>
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="https://www.ladrilar.com.br/wp-content/uploads/2020/12/cinza-escuro.png" class="img-fluid rounded-start" alt="...">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h3 class="card-title text-center">Título</h3>
-                            <h4 class="card-text text-center">Sub. Título</h4>
-                            <a href="#" class="stretched-link"></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
     <?php
     include_once '_footer.php';
     ?>
