51be5f9617a5aa2c4cbc2f518288fa4ee0accda0
MauroMFP <mauricioferreiraprederigo@gmail.com>
2022-11-23 18:55:46 -0300

    Editoria Quase Pronta

--- editoria.php
+++ editoria.php
@@ -25,7 +25,29 @@
             </div>
         <hr>
         <div id='div2'>
-           
+            <div class='list-group-horizontal'>
+                <div id='div4'>
+                    <div>
+                        <input type="text" name="tipo" id='tipo' size="20" maxlength="10">
+                        <hr>
+                        <input type="number" name="conversor" id='conversor' size="20">
+                    </div>
+                        <h1>=</h1>
+                    <div>
+                        <input type="text" name="tipo" id='tipo' size="20" maxlength="10">
+                        <hr>
+                        <input type="number" name="conversor" id='conversor' size="20">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div id='div5'>
+            <h2 id='h2'>Como Funciona?</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus ex sunt? Eveniet, veritatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus ex sunt? Eveniet, veritatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus ex sunt? Eveniet, veritatis!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus ex sunt? Eveniet, veritatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus ex sunt? Eveniet, veritatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus ex sunt? Eveniet, veritatis!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus ex sunt? Eveniet, veritatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus ex sunt? Eveniet, veritatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus ex sunt? Eveniet, veritatis!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus ex sunt? Eveniet, veritatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus ex sunt? Eveniet, veritatis! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, sequi reprehenderit consequatur maiores minima praesentium vero soluta. Ullam ducimus, omnis sit reiciendis quaerat laudantium consectetur temporibus ex sunt? Eveniet, veritatis!</p>
         </div>
     </main>
     <script src="./assets/script.js"></script>
