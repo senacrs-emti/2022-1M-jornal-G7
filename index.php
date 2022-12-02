286751038f1a11d6e39516b134ee87118d2deb8a
EthanSchwartzhaupt <ethan29ssr@gmail.com>
2022-12-02 10:47:24 -0300

    Complexo do main
    main

--- index.php
+++ index.php
@@ -2,13 +2,75 @@
     include_once "_header.php"
 ?>
 <main class=".container">
-    <div class="row">
-        <div class="col-sm-2" id="main_index">
+    <div class="row justify-content-center pt-5 pb-5">
+        <div id="carouselExampleIndicators" class="carousel slide w-50 h-auto" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img class="d-block w-100" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Aspect-ratio-16x9.svg/1200px-Aspect-ratio-16x9.svg.png" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Aspect-ratio-16x9.svg/1200px-Aspect-ratio-16x9.svg.png" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Aspect-ratio-16x9.svg/1200px-Aspect-ratio-16x9.svg.png" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <div class="row justify-content-center pb-5 pt-3">
+        <div class="col-md-4">
             <img class="w-100 h-auto" src="https://www.aspect-ratios.com/file/2020/12/4-3-aspect-ratio.png" alt="">
         </div>
         <div class="col-6">
+            <h1 class="...">Titulo</h1>
+            <h3>Sub titulo</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, quasi excepturi ut tenetur, cupiditate modi vel quos sed voluptates dolore ratione illo nihil corrupti impedit saepe. Atque enim voluptate nobis!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam dolorum aperiam eveniet quae commodi quod voluptatibus officiis fugit accusantium est. Voluptas natus maiores assumenda sequi quos, alias minima sapiente sint?</p>
+        </div>
+    </div>
+    <div class="row p-4">
+        <div class="col-sm-2" id="main_index">
+            <img class="w-100 h-auto" src="https://www.aspect-ratios.com/file/2020/12/4-3-aspect-ratio.png" alt="">
+        </div>
+        <div class="col-4">
             <h1>Titulo</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quae voluptatibus eveniet mollitia iusto. Debitis mollitia saepe sint esse optio, facere tempora error delectus tenetur excepturi adipisci! Nisi, iure iste.</p>
+        </div>
+    </div>
+    <div class="row p-4">
+        <div class="col-sm-2" id="main_index">
+            <img class="w-100 h-auto" src="https://www.aspect-ratios.com/file/2020/12/4-3-aspect-ratio.png" alt="">
+        </div>
+        <div class="col-4">
+            <h1>Titulo</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quae voluptatibus eveniet mollitia iusto. Debitis mollitia saepe sint esse optio, facere tempora error delectus tenetur excepturi adipisci! Nisi, iure iste.</p>
+        </div>
+    </div>
+    <div class="row p-4">
+        <div class="col-sm-2" id="main_index">
+            <img class="w-100 h-auto" src="https://www.aspect-ratios.com/file/2020/12/4-3-aspect-ratio.png" alt="">
+        </div>
+        <div class="col-4">
+            <h1>Titulo</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quae voluptatibus eveniet mollitia iusto. Debitis mollitia saepe sint esse optio, facere tempora error delectus tenetur excepturi adipisci! Nisi, iure iste.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <h2>Moedas</h2>
         </div>
     </div>
 </main>
