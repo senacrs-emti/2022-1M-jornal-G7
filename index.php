4df15477e5cd641d7f3b76607c8b843d6a8bf8d3
EthanSchwartzhaupt <ethan29ssr@gmail.com>
2022-12-02 09:06:57 -0300

    Começo real do index

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
     include_once "_header.php"
 ?>
-<main class="container-fluid">
-    <section class="row">
-        <div class="col-lg-8">
-            <section class="col-lg-3"></section>
+<main class=".container">
+    <div class="row">
+        <div class="col-sm-2" id="main_index">
+            <img class="w-100 h-auto" src="https://www.aspect-ratios.com/file/2020/12/4-3-aspect-ratio.png" alt="">
         </div>
-    </section>
+        <div class="col-6">
+            <h1>Titulo</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quae voluptatibus eveniet mollitia iusto. Debitis mollitia saepe sint esse optio, facere tempora error delectus tenetur excepturi adipisci! Nisi, iure iste.</p>
+        </div>
+    </div>
 </main>
+<?php 
+    include_once "_footer.php"
+?>
